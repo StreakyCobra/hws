1c706f1150754872a084264f39ee82a13ee75b19
Fabien Dubosson <fabien.dubosson@gmail.com>
2015-03-23 16:30:04 +0100

    Improve comments in version.sh

--- version.sh
+++ version.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 #
-# Return the version number in the form:
+# Returns the version number in the form:
 #
 #   MAJOR.MINOR.PATCH
 #
 # from the version declared in the «myocamlbuild.ml» file. It permits to get the
-# version number from scripts and Makefiles.
+# version number from scripts and Makefiles by keeping the version number stored
+# in one central place.
 
 cat myocamlbuild.ml \
   | grep -e '^let \(major\|minor\|patch\)' \
