74ce7231760789f59a0852b3ef687438b45baa39
Fabien Dubosson <fabien.dubosson@gmail.com>
2015-03-23 16:08:17 +0100

    Add comment in version.sh

--- version.sh
+++ version.sh
@@ -1,3 +1,12 @@
+#!/usr/bin/env bash
+#
+# Return the version number in the form:
+#
+#   MAJOR.MINOR.PATCH
+#
+# from the version declared in the «myocamlbuild.ml» file. It permits to get the
+# version number from scripts and Makefiles.
+
 cat myocamlbuild.ml \
   | grep -e '^let \(major\|minor\|patch\)' \
   | cut -f2 -d= \
